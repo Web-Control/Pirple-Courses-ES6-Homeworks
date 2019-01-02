d36484662b505435cdc4be9c1e02de66fed08748
Szymon Chomej <szymon@web-control.pl>
2019-01-02 09:05:48 +0100

    Favourite song

---
+++ song.js
@@ -0,0 +1,26 @@
+/*
+Title: One of My favourite songs.
+Author: Szymon Chomej.
+Description:In this file i describe one of my favourite song by few variables.
+*/
+
+var title = "Małomiasteczkowy";
+var artist = "Dawid Podsiadło";
+var genre = "Pop"; // could be "indie pop"
+var lyricsAuthor = "Dawid Podsiadło"
+var composer = "Bartosz Dziedzic, Dawid Podsiadło";
+var durationInSeconds = 204;
+var yearReleased = 2018;
+var loveIt = true;
+
+//We console.log all variables
+
+console.log(title);
+console.log(artist);
+console.log(genre);
+console.log(lyricsAuthor);
+console.log(composer);
+console.log(durationInSeconds);
+console.log(yearReleased);
+console.log(loveIt);
+
